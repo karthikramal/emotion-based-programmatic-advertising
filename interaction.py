cf139c9bad6df34d477077cb0390c5db6d1098ea
karthikramal <153757585+karthikramal@users.noreply.github.com>
2023-12-15 10:40:55 -0500

    Update interaction.py

--- interaction.py
+++ interaction.py
@@ -1,14 +1,6 @@
 import mimetypes
-
-def recognize_image(image_path):
-    # Placeholder function for image recognition
-    # In a real scenario, you would use an image recognition model or API here
-    return "Recognized content of the image"
-
-def recognize_video(video_path):
-    # Placeholder function for video recognition
-    # In a real scenario, you would use a video recognition model or API here
-    return "Recognized content of the video"
+from image_recognition import process_image
+from video_recognition import process_video
 
 def main(file_path):
     # Determine the type of the file based on MIME type
@@ -16,9 +8,9 @@ def main(file_path):
 
     if file_type is not None:
         if file_type.startswith('image'):
-            return recognize_image(file_path)
+            return process_image(file_path)
         elif file_type.startswith('video'):
-            return recognize_video(file_path)
+            return process_video(file_path)
         else:
             return "File is neither an image nor a video"
     else:
